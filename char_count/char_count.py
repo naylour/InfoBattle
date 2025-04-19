21d763db8c3072a54094178a9622a4dd8f1f9d00
Naylor <ShahTheShah@ya.ru>
2025-04-19 11:55:30 +0300

    update

--- char_count/char_count.py
+++ char_count/char_count.py
@@ -1,9 +1,9 @@
 from functools import reduce
 
-def char_count(text: str):
+def char_count(text: str) -> int:
     text = text.lower()
 
-    chars_count = reduce(lambda acc, char: {**acc, char: acc.get(char, 0) + 1}, text, {})
+    chars_count: dict[str, int] = reduce(lambda acc, char: {**acc, char: acc.get(char, 0) + 1}, text, {})
 
     filtered_chars = {char: count for char, count in chars_count.items() if count > 1}
 
